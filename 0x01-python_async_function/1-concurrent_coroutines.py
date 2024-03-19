f8da165af847b25b799a23aaee8183d89c614937
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-19 02:25:25 +0200

    fixed pycode

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,11 @@
 import asyncio
 from typing import List
 
+wait_random = __import__('0-basic_async_syntax').wait_random
 
-delay = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Asynchronous coroutine that takes in an integer argument (n) and a"""
     """integer argument (max_delay, with a default value of 10) named"""
-    delays = [delay(max_delay) for _ in range(n)]
+    delays = [wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in asyncio.as_completed(delays)]
