201df9ff24946bb601ee4f63d25fbc4ba47efe6c
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-20 03:06:18 +0200

    fixed the loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """ Coroutine that loops 10 times, each time yielding a random number """
-    for i in range(10):
+    for _ in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
