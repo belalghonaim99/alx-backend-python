e8c3dcbc5e6d7dbe047c766219e31792eb35c3cd
BelalGhonaim <belalpop1999@yahoo.com>
2024-03-20 03:12:48 +0200

    updated

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ 0. Async Generator """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Coroutine that loops 10 times, each time yielding a random number """
     for _ in range(0, 10):
         yield random.uniform(0, 10)
