0f9ad620486ceeda36706f06121ec32299e1956c
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-31 22:07:06 +0300

    add blank lines

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -143,6 +143,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
 
+
     def test_public_repos(self) -> None:
         """Tests the `public_repos` method"""
         self.assertEqual(
@@ -150,12 +151,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             self.expected_repos,
         )
 
+
     def test_public_repos_with_license(self) -> None:
         """Tests the `public_repos` method with a license argument."""
         self.assertEqual(
             GithubOrgClient("google").public_repos(license="apache-2.0"),
             self.apache2_repos,
         )
+
 
     @classmethod
     def tearDownClass(cls) -> None:
