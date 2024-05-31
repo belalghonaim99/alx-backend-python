634a4c2e431f2841562d41168c5502fd8825f1ae
BelalGhonaim <belalpop1999@yahoo.com>
2024-05-31 22:20:07 +0300

    fixed pycdoe

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -117,32 +117,36 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client_with_licence, expect)
 
 """ Integration test for the `GithubOrgClient` class."""
+
+
 @parameterized_class([
     {
         'org_payload': TEST_PAYLOAD[0][0],
         'repos_payload': TEST_PAYLOAD[0][1],
         'expected_repos': TEST_PAYLOAD[0][2],
         'apache2_repos': TEST_PAYLOAD[0][3],
-    },
+    },-
 ])
 class TestIntegrationGithubOrgClient(unittest.TestCase):
     """ Tests the `GithubOrgClient` class."""
     @classmethod
     def setUpClass(cls) -> None:
         """Sets up class fixtures before running tests."""
-        routePayload = {
+        route_payload = {
             'https://api.github.com/orgs/google': cls.org_payload,
             'https://api.github.com/orgs/google/repos': cls.repos_payload,
         }
         """ Integration test for the `GithubOrgClient` class."""
+
         def get_payload(url):
-            if url in routePayload:
-                return Mock(**{'json.return_value': routePayload[url]})
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
             return HTTPError
 
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
 
+    """Integration test for the `GithubOrgClient` class."""
 
     def test_public_repos(self) -> None:
         """Tests the `public_repos` method"""
@@ -150,8 +154,6 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             GithubOrgClient("google").public_repos(),
             self.expected_repos,
         )
-
-
     def test_public_repos_with_license(self) -> None:
         """Tests the `public_repos` method with a license argument."""
         self.assertEqual(
@@ -162,5 +164,5 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
 @classmethod
 def tearDownClass(cls) -> None:
-        """Cleans up class fixtures after running tests."""
-        cls.get_patcher.stop()
+    """Cleans up class fixtures after running tests."""
+    cls.get_patcher.stop()
